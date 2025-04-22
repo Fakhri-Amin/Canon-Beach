20c42042d2ef801d1ba069faeb039f5920d2ef1d
Farou <86537710+Fakhri-Amin@users.noreply.github.com>
2025-04-22 21:05:02 +0900

    Add explosion effect

---
+++ Canon Beach/Assets/Scripts/ExplosiveShot.cs
@@ -0,0 +1,27 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ExplosiveShot : Shot
+{
+    [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] private float explosionForce = 3f;
+    [SerializeField] private float upwardForce = 1f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        var rigidBodyColliders = Physics.OverlapSphere(collision.contacts[0].point, explosionRadius);
+
+        foreach (var item in rigidBodyColliders)
+        {
+            if (item.CompareTag("Target"))
+            {
+                item.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, collision.contacts[0].point, upwardForce, upwardForce, ForceMode.Impulse);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
